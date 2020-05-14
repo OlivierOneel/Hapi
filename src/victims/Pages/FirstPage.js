79908b82238ebeac5ea17aa5e1edbb5411d3cdaf
olivier.oneel <onopsp@pckswarms.ch>
2020-05-14 10:01:23 -0400

    initial_victim_branch

--- src/victims/Pages/FirstPage.js
+++ src/victims/Pages/FirstPage.js
@@ -3,10 +3,25 @@ import {Route, Switch} from "react-router-dom"
 import SecondPage from "./SecondPage"
 
 class FirstPage extends React.Component{
+    constructor (){
+        super()
+        this.state={
+            foodLevel: "2"
+        }
+    }
+    
     render(){
+        const imageClick = () => {
+            this.state.foodLevel=1
+            alert(this.state.foodLevel)
+          } 
     return(
         <div>
-            <h1>hello Victims</h1>
+            <h1>We are here to help you</h1>
+            <p>How much food do you have left?</p>
+            <img src={require("../../images/full_food.png")} onClick={() => imageClick()}/>
+            <img src={require("../../images/some_food.png")} onClick={() => imageClick()}/>
+            <img src={require("../../images/no_food.png")} onClick={() => imageClick()}/>
             <button onClick={event =>window.location.href="/victims/2"}>
                 Next Page
             </button>
