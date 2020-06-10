ee8b05e89ec3354e369f39683f7062090ca5d8b5
olivier.oneel <onopsp@pckswarms.ch>
2020-06-10 12:20:25 -0400

    update_4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component }from 'react';
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-/* Componentns import */
-import mainPage from './components/mainPage'
-import helpFirstPage from './components/helper/helpFirstPage'
-import victimFirstPage from './components/victim/victimFirstPage'
+ 
+import VictimsFirst from "./victims/Pages/FirstPage"
+import HelpFirst from "./help/Pages/FirstPage"
+import mainPage from "./components/mainPage"
+import VictimsSecond from "./victims/Pages/SecondPage"
+import VictimsLast from "./victims/Pages/LastPage"
+ 
 
 
 class App extends React.Component{
