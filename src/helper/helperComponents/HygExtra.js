a6e693b048d5d80eaec85aa745897075fb6cc406
olivier.oneel <onopsp@pckswarms.ch>
2020-07-13 20:57:34 -0400

    correction_5

--- src/helper/helperComponents/HygExtra.js
+++ src/helper/helperComponents/HygExtra.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const HygExtra = () => {
 
-    const [hygieneState, setHygiene ] = useState([
+    const [hygieneState, setHygiene] = useState([
         { isChecked: false, label: "Toothbrush", id: 0 },
         { isChecked: false, label: "Toothpaste", id: 1 },
         { isChecked: false, label: "Toilet Paper", id: 2 },
@@ -13,29 +13,41 @@ const HygExtra = () => {
         { isChecked: false, label: "Sanitary Pad", id: 6 }
     ])
 
+/*     const tester = (e) => {
+        e.preventDefault()
+        console.log(hygieneState)
+    } */
+
+    const selectHandler = (item) => {
+        let currentState = item;
+        currentState.isChecked = !item.isChecked
+        setHygiene([
+            ...hygieneState
+        ])
+        /* console.log(hygieneState) */
+
+}
 
     return (
         <div>
             <h2>Please check the items you can help with:</h2>
-            {hygieneState.map(item => (
-                <div key={item.id}>
-                    <label key={item.id}>{item.label}
-                        <input
-                            className='checkbox'
-                            type="checkbox"
-                            key={item.id}
-                            checked={item.isChecked}
-                            /* onChange={(e)=> {checkHandler(e,item.id)}} */ 
-                            /* onChange={(e)=>setHygiene(
-                                [...hygieneState].map(obj =>{
-                                    if(obj.id===item.id) {
-                                        obj.isChecked=e.target.checked
-                                    }
-                                }))} */
-                        />
-                    </label>
-                </div>
-            ))}
+            <div>
+                {hygieneState.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            <label key={item.id}>{item.label}
+                                <input
+                                    type="checkbox"
+                                    key={item.id}
+                                    onChange={()=>selectHandler(item)}
+                                    checked={item.isChecked}
+                                />
+                            </label>
+                        </div>
+                    )
+                    })}
+            </div>
+    )
         </div>
     )
 }
