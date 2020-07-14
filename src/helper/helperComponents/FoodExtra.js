b84776b22577d061d2eacaa7badbadf6fd1818cc
olivier.oneel <onopsp@pckswarms.ch>
2020-07-13 20:56:37 -0400

    correction_4

--- src/helper/helperComponents/FoodExtra.js
+++ src/helper/helperComponents/FoodExtra.js
@@ -6,31 +6,15 @@ const FoodExtra = (props) => {
         canned: false,
         babyFood: false,
         vegetables: false,
-        meat: false
     });
-
-    const foodTypeSubmit = (e) => {
-        e.preventDefault()
-        console.log(foodSelection)
-    }
-
-    const stateReset = (e) => {
-        e.preventDefault()
-        setFoodSelection({
-            canned: false,
-            babyFood: false,
-            vegetables: false,
-            meat: false
-        })
-    }
 
     return (
         <div>
             <h2>Please select the type of food you can offer:</h2>
-            <p>{foodSelection.canned}</p>
+            
+            
                 <label >Canned Food
                 <input
-                        className='checkbox'
                         type="checkbox"
                         id="canned"
                         checked={foodSelection.canned}
@@ -38,7 +22,6 @@ const FoodExtra = (props) => {
                 </label>
                 <label >Baby Food
                 <input
-                        className='checkbox'
                         type="checkbox"
                         id="babyFood"
                         checked={foodSelection.babyFood}
@@ -46,12 +29,12 @@ const FoodExtra = (props) => {
                 </label>
                 <label >Vegetables
                 <input
-                        className='checkbox'
                         type="checkbox"
                         id="vegetables"
                         checked={foodSelection.vegetables}
                         onChange={(e) => { setFoodSelection({ ...foodSelection,vegetables: e.target.checked }) }} />
                 </label>
+            
         </div>
     );
 };
