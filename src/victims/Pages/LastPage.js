12592172d749b77765162065bc4440a1317205a6
Alex Caissy <alexcaissy@MacBook-Pro.local>
2020-07-22 15:52:12 -0400

    Ajout de GeoLoc

--- src/victims/Pages/LastPage.js
+++ src/victims/Pages/LastPage.js
@@ -1,14 +1,35 @@
 import React from 'react';
- 
-class LastPage extends React.Component{
+import GeoLocation from '../../helper/helperComponents/GeoLocation';
 
-   render(){
-    return (
-       <div>
-          <h1>Help will be sent</h1>
-       </div>
-    );
+class LastPage extends React.Component {
+
+   constructor(props) {
+      super(props);
+      this.state = true;
+      this.allowButton = this.allowButton.bind(this)
+      this.helpSent = this.helpSent.bind(this)
+   }
+
+   allowButton() {
+      this.setState();
+   };
+
+   helpSent() {
+      alert("Help will be sent!")
+   }
+
+   render() {
+
+      
+
+      return (
+         <div>
+            <h1>Let us get your location.</h1>
+            <GeoLocation/> //Ça c'est le comp. qui trouve la geoloc. Et le 'disabled=this.state' met le boutton disable if true.
+            <button disabled={this.state} className="FirstPageButton" onClick={this.helpSent}>Submit</button> //disabled={this.state} c'est ça qui devrait rendre le bouton active
+         </div>
+      );
+   }
 }
-}
- 
+
 export default LastPage;
