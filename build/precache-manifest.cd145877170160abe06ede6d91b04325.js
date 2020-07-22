439d0c728d3f062fca7207e4aba433b8ed4d9bcf
Alex Caissy <alexcaissy@MacBook-Pro.local>
2020-07-22 18:10:32 -0400

    Ajout GeoLoc AVEC BUILD

---
+++ build/precache-manifest.cd145877170160abe06ede6d91b04325.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c041b747b10d455ca09dc753e60540a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed62af183a48cca210c4",
+    "url": "/static/css/main.c9dd723e.chunk.css"
+  },
+  {
+    "revision": "0ece0cc31f37386f1c3a",
+    "url": "/static/js/2.ac9c4be5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ac9c4be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed62af183a48cca210c4",
+    "url": "/static/js/main.e8156c64.chunk.js"
+  },
+  {
+    "revision": "286bed470230ef6fcad2",
+    "url": "/static/js/runtime-main.643e38b8.js"
+  },
+  {
+    "revision": "7e3b90e034cee905b5bbfc1c542e95fa",
+    "url": "/static/media/adult.7e3b90e0.png"
+  },
+  {
+    "revision": "2b4423ce95e3bf3d0ac34d3997c9ecae",
+    "url": "/static/media/background.2b4423ce.jpg"
+  },
+  {
+    "revision": "c999a0a85db112ead62f01dfe26de5f6",
+    "url": "/static/media/boy.c999a0a8.png"
+  },
+  {
+    "revision": "95318d7c4c4f2480aa3d0dd108d9f547",
+    "url": "/static/media/bucket.95318d7c.png"
+  },
+  {
+    "revision": "976763da46c2a2a81851c458fdaa6532",
+    "url": "/static/media/camping.976763da.png"
+  },
+  {
+    "revision": "19cf2a0bc26610badde14a8cc6f385b9",
+    "url": "/static/media/clothes.19cf2a0b.png"
+  },
+  {
+    "revision": "48804ec1e73c709cdf49421de4937512",
+    "url": "/static/media/food_full.48804ec1.png"
+  },
+  {
+    "revision": "1f2406e2b26f252d6298009e29f937dd",
+    "url": "/static/media/food_no.1f2406e2.png"
+  },
+  {
+    "revision": "7628ed6fa4436640aebd1190d275740d",
+    "url": "/static/media/food_some.7628ed6f.png"
+  },
+  {
+    "revision": "ea5b239156d4e2dfa8c1ceb970e13792",
+    "url": "/static/media/medecine.ea5b2391.png"
+  },
+  {
+    "revision": "59193ad1b42aa30ecea65ef046c0baeb",
+    "url": "/static/media/pet-food.59193ad1.png"
+  },
+  {
+    "revision": "a41901cf948a9ef9980305c78a860b97",
+    "url": "/static/media/pets.a41901cf.png"
+  },
+  {
+    "revision": "c5c81bf9cfd9c6d4cfb52a061d8b3c05",
+    "url": "/static/media/rope.c5c81bf9.png"
+  },
+  {
+    "revision": "cd25298ffe19a4b97af80e040b8a543f",
+    "url": "/static/media/soap.cd25298f.png"
+  },
+  {
+    "revision": "7746122c55362c16d8c1acc459cba990",
+    "url": "/static/media/toilet-paper.7746122c.png"
+  },
+  {
+    "revision": "82a1da61ea6c6c72cecc267311097812",
+    "url": "/static/media/toilet.82a1da61.png"
+  },
+  {
+    "revision": "a03d799ff9d0b2af5dc19045714f88f9",
+    "url": "/static/media/toothbrush.a03d799f.png"
+  }
+]);
